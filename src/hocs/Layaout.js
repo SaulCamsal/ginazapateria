5a2a38b4e64a16cb41af42d818235bec00eb1a3d
Saul_Camsa <saulcam_s01@hotmail.com>
2023-11-18 12:27:52 -0600

    Try collectStatic for Django

--- src/hocs/Layaout.js
+++ src/hocs/Layaout.js
@@ -3,11 +3,14 @@ import 'react-toastify/dist/ReactToastify.css';
 //Export con brakets {}, porq nuestro componente no va definido como un "export default"
 import Navbar from "../componets/navigation/Navbar";
 import Footer from "../componets/navigation/Footer";
+import Display from "../componets/navigation/Display";
+import Cart from "../componets/navigation/Cart";
 
 const Layout = (props) => {
     return(
         <div>
             <Navbar/>
+            <Cart/>
             <ToastContainer autoClose={5000} /> 
             {props.children}
             <Footer/>
